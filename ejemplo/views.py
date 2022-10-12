0df10ca9c4e144a38babfcfdf5c5fddcc6b6ba65
vcodega <valentincodega@gmail.com>
2022-10-11 23:26:44 -0300

    agrego novedades

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    return render(request, "ejemplo/saludar.html", {"nombre":"Valentin"})
+
+def index_tres(request):
+    return render(request, "ejemplo/saludar.html", 
+    {"notas": [1,2,3,4,5,6]}
+    )
--- project/urls.py
+++ project/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ejemplo.views import index
+from ejemplo.views import index, index_tres
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludar/', index),
+    path('mostrar-notas/',index_tres),
 ]

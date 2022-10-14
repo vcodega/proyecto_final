e5b56272511be018a4b1d933af2284a1472df09a
vcodega <valentincodega@gmail.com>
2022-10-13 23:33:02 -0300

    Aplicación Blog + template

--- project/urls.py
+++ project/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from ejemplo.views import index, index_tres, monstrar_familiares
+from blog.views import index as blog_index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludar/', index),
     path('mostrar-notas/',index_tres),
     path('mi-familia/', monstrar_familiares), 
+    path('blog/',blog_index),
 ]
